64f8df9c5ab8b2cd4f5c29b7bcd8938d542cbe63
relifevn <thinhhuynh3108@gmail.com>
2019-10-20 16:29:44 +0700

    Add new layout

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,95 @@ const express = require('express');
 const router = express.Router();
 const database = require('../routes/database');
 const emitSockets = require('../websocket/socket').emitSockets;
+const nodeMailer = require('nodemailer');
 
 const API_KEY = 'F72FD054C190F505B93F09690BA99C5B';
 const TITLE = 'KIDSAFE';
 const VERSION = 'v0.0.2';
+const USER_GMAIL = 'helonesecure@gmail.com';
+const PASS_GMAIL = '50BD1167F23A9AD9673FD350B64B21BC';
+const SENT_TO_GMAIL = 'thinhhuynh3108@gmail.com';
+
+const sendMail = (data) => {
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: USER_GMAIL,
+      pass: PASS_GMAIL
+    }
+  });
+  let mailOptions = {
+    from: '"SafeKid system" <helonesecure@gmail.com>', // sender address
+    to: SENT_TO_GMAIL, // list of receivers
+    subject: '[Alert] auto-alert from SafeKid system', // Subject line
+    text: '', // plain text body
+    html: `
+      <b>Detect danger in vehicle ${data.vehicleId} in school ${data.schoolId} at ${new Date()} </b>
+      <p>Please don't reply!</p>
+    ` // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: TITLE, version: VERSION });
+  database.ref('/').once('value').then(function (snapshot) {
+    const value = snapshot.val();
+    const totalSchools = Object.keys(value).length;
+    const totalVehicles = Object.keys(value).map(key => Object.keys(value[key].vehicles).length).reduce((a, b) => a + b, 0);
+    const vehicles = [].concat(...Object.keys(value).map(key => {
+      const vehicles = value[key].vehicles;
+      return Object.keys(vehicles).map(subKey => ({
+        ...vehicles[subKey],
+        schoolName: value[key].name,
+        noData: Object.keys(vehicles[subKey].statuses).length,
+        vstateClass: vehicles[subKey].vstate === 'Active' ? 'complete' : 'pending'
+      }));
+    }));
+    const statuses = [].concat(...vehicles.map(vehicle => {
+      const statuses = vehicle.statuses;
+      return Object.keys(statuses).map(subKey => ({
+        ...statuses[subKey],
+        schoolName: vehicle.schoolName,
+        vehicleId: vehicle.vid,
+        statusName: statuses[subKey].isHasPerson === 'true' ? 'Danger!' : ''
+      }));
+    }));
+    res.render('home', { totalSchools: totalSchools, totalVehicles: totalVehicles, vehicles: vehicles, statuses: statuses });
+  });
 });
 
-router.get('/home', (req, res, next) => {
-  res.render('home', {});
+router.get('/home', async (req, res, next) => {
+  database.ref('/').once('value').then(function (snapshot) {
+    const value = snapshot.val();
+    const totalSchools = Object.keys(value).length;
+    const totalVehicles = Object.keys(value).map(key => Object.keys(value[key].vehicles).length).reduce((a, b) => a + b, 0);
+    const vehicles = [].concat(...Object.keys(value).map(key => {
+      const vehicles = value[key].vehicles;
+      return Object.keys(vehicles).map(subKey => ({
+        ...vehicles[subKey],
+        schoolName: value[key].name,
+        noData: Object.keys(vehicles[subKey].statuses).length,
+        vstateClass: vehicles[subKey].vstate === 'Active' ? 'complete' : 'pending'
+      }));
+    }));
+    const statuses = [].concat(...vehicles.map(vehicle => {
+      const statuses = vehicle.statuses;
+      return Object.keys(statuses).map(subKey => ({
+        ...statuses[subKey],
+        schoolName: vehicle.schoolName,
+        vehicleId: vehicle.vid,
+        statusName: statuses[subKey].isHasPerson === 'true' ? 'Danger!' : ''
+      }));
+    }));
+    res.render('home', { totalSchools: totalSchools, totalVehicles: totalVehicles, vehicles: vehicles, statuses: statuses });
+  });
 });
 
 router.get('/index', (req, res, next) => {
@@ -118,6 +196,10 @@ router.get('/widgets', (req, res, next) => {
   res.render('widgets', {});
 });
 
+router.get('/school', (req, res, next) => {
+  res.render('school', {});
+});
+
 router.get('/upload', (req, res, next) => {
   var vehicleId = req.query.vehicleId || '';
   var schoolId = req.query.schoolId || '';
@@ -141,7 +223,11 @@ router.get('/upload', (req, res, next) => {
           isHasPerson: isHasPerson
         };
         database.ref(`/${schoolId}/vehicles/${vehicleId}/statuses`).update(data);
-        emitSockets('alert', { vehicleId: vehicleId, schoolId: schoolId, isHasPerson: isHasPerson, data: data });
+        const _data = { vehicleId: vehicleId, schoolId: schoolId, isHasPerson: isHasPerson, date: new Date() };
+        emitSockets('alert', { vehicleId: vehicleId, schoolId: schoolId, isHasPerson: isHasPerson, data: _data });
+        if (isHasPerson === 'true') {
+          sendMail(_data);
+        }
         res.json({ status: 1, message: 'ok!' });
       } else {
         res.json({ status: 0, error: { message: 'vehicleId or schoolId is wrong!' } });
