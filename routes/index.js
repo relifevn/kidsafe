d03d054074e3b8f21962fa31ebb2a87e89730633
relifevn <thinhhuynh3108@gmail.com>
2019-10-20 11:13:17 +0700

    init layout

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,115 @@ const API_KEY = 'F72FD054C190F505B93F09690BA99C5B';
 const TITLE = 'KIDSAFE';
 const VERSION = 'v0.0.2';
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: TITLE, version: VERSION }); // index.hbs file is rendered
+  res.render('index', { title: TITLE, version: VERSION });
+});
+
+router.get('/home', (req, res, next) => {
+  res.render('home', {});
+});
+
+router.get('/index', (req, res, next) => {
+  res.render('index', {});
+});
+
+router.get('/charts-chartjs', (req, res, next) => {
+  res.render('charts-chartjs', {});
+});
+
+router.get('/charts-flot', (req, res, next) => {
+  res.render('charts-flot', {});
+});
+
+router.get('/charts-peity', (req, res, next) => {
+  res.render('charts-peity', {});
+});
+
+router.get('/font-fontawesome', (req, res, next) => {
+  res.render('font-fontawesome', {});
+});
+
+router.get('/font-themify', (req, res, next) => {
+  res.render('font-themify', {});
+});
+
+router.get('/forms-advanced', (req, res, next) => {
+  res.render('forms-advanced', {});
+});
+
+router.get('/forms-basic', (req, res, next) => {
+  res.render('forms-basic', {});
+});
+
+router.get('/maps-gmap', (req, res, next) => {
+  res.render('maps-gmap', {});
+});
+
+router.get('/maps-vector', (req, res, next) => {
+  res.render('maps-vector', {});
+});
+
+router.get('/page-login', (req, res, next) => {
+  res.render('page-login', {});
+});
+
+router.get('/page-register', (req, res, next) => {
+  res.render('page-register', {});
+});
+
+router.get('/pages-forget', (req, res, next) => {
+  res.render('pages-forget', {});
+});
+router.get('/tables-basic', (req, res, next) => {
+  res.render('tables-basic', {});
+});
+
+router.get('/tables-data', (req, res, next) => {
+  res.render('tables-data', {});
+});
+
+router.get('/ui-alerts', (req, res, next) => {
+  res.render('ui-alerts', {});
+});
+
+router.get('/ui-badges', (req, res, next) => {
+  res.render('ui-badges', {});
+});
+
+router.get('/ui-buttons', (req, res, next) => {
+  res.render('ui-buttons', {});
+});
+
+router.get('/ui-cards', (req, res, next) => {
+  res.render('ui-cards', {});
+});
+
+router.get('/ui-grids', (req, res, next) => {
+  res.render('ui-grids', {});
+});
+
+router.get('/ui-modals', (req, res, next) => {
+  res.render('ui-modals', {});
+});
+
+router.get('/ui-progressbar', (req, res, next) => {
+  res.render('ui-progressbar', {});
+});
+
+router.get('/ui-switches', (req, res, next) => {
+  res.render('ui-switches', {});
+});
+
+router.get('/ui-tabs', (req, res, next) => {
+  res.render('ui-tabs', {});
+});
+
+router.get('/ui-typgraphy', (req, res, next) => {
+  res.render('ui-typgraphy', {});
+});
+
+router.get('/widgets', (req, res, next) => {
+  res.render('widgets', {});
 });
 
 router.get('/upload', (req, res, next) => {
